9b88c05b0950a6f3e9904253423e86b39e90d740
imaginaryfriend <194876759+notyourimaginarycoder@users.noreply.github.com>
2025-05-05 17:04:19 +0100

    auto commit: Mon  5 May 17:04:19 BST 2025

--- src/components/projects.ts
+++ src/components/projects.ts
@@ -15,25 +15,11 @@ const projects: Project[] = [
     repoLink: 'https://github.com/notyourimaginarycoder/portfolio',
   },
   {
-    title: 'Portfolio',
+    title: 'StackDew Valley',
     description: 'A simple portfolio created with TypeScript and Vite',
     imageUrl: 'https://via.placeholder.com/300x200',
     liveLink: 'https://notyourimaginarycoder.github.io/',
-    repoLink: 'https://github.com/notyourimaginarycoder/portfolio',
-  },
-  {
-    title: 'Portfolio',
-    description: 'A simple portfolio created with TypeScript and Vite',
-    imageUrl: 'https://via.placeholder.com/300x200',
-    liveLink: 'https://notyourimaginarycoder.github.io/',
-    repoLink: 'https://github.com/notyourimaginarycoder/portfolio',
-  },
-  {
-    title: 'Portfolio',
-    description: 'A simple portfolio created with TypeScript and Vite',
-    imageUrl: 'https://via.placeholder.com/300x200',
-    liveLink: 'https://notyourimaginarycoder.github.io/',
-    repoLink: 'https://github.com/notyourimaginarycoder/portfolio',
+    repoLink: 'https://github.com/notyourimaginarycoder/stackdew-valley',
   },
 ]
 
